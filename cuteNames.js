5da65729ef37cf6577d0c9f24282c08fd09dd0a1
Meowthebest <138353441+Meowthebest@users.noreply.github.com>
2024-10-04 00:35:56 +0400

    Update cuteNames.js

--- cuteNames.js
+++ cuteNames.js
@@ -42,17 +42,47 @@ const cuteNames = {
         "SleepyPuff", "MilkTeaMochi", "CottonCandyKiss", "MochiMoon", "JellyBeanGiggles", 
         "ButtonBunny", "CinnamonCloud", "DreamyBubbles", "SparkleHoney", "TwinkleBerry"
     ],
-    clean: [
-        "Berry", "Cherry", "Kiwi", "Peach", "Lemon", "Blueberry", "Grape", "Melon", "Citrus", "Mango",
-        "Strawberry", "Watermelon", "Pineapple", "Apple", "Pear", "Plum", "Lychee", "Orange", "Coconut", "Banana",
-        "Kitty", "Whiskers", "Snow", "Cuddle", "Fluffy", "Neko", "Meow", "Whisker", "Moonlight", "Tail",
-        "Sakura", "Kawaii", "Mochi", "Heart", "Dreamer", "Star", "Senpai", "Yume", "Kitsune", "Amai",
-        "Lux", "Ahri", "Jinx", "Ezreal", "Yasuo", "Riven", "Katarina", "Poppy", "Zoe", "MissFortune",
-        "Venti", "Klee", "Diluc", "Childe", "Lumine", "Xiao", "Jean", "Ganyu", "Keqing", "Noelle",
-        "HelloKitty", "MyMelody", "Cinnamoroll", "Pompompurin", "Kuromi", "Keroppi", "LittleTwinStars",
-        "Bubble", "Cupcake", "Sugar", "Starry", "Glitter", "Marshmallow", "Rainbow", "Fairy", "Bunny",
-        "Sleepy", "Milk", "Cotton", "Jelly", "Button", "Cinnamon", "Dreamy", "Sparkle"
-    ]
+clean: [
+    "Berry", "Cherry", "Kiwi", "Peach", "Lemon", "Blueberry", "Grape", "Melon", "Citrus", "Mango",
+    "Strawberry", "Watermelon", "Pineapple", "Apple", "Pear", "Plum", "Lychee", "Orange", "Coconut", "Banana",
+    "Kitty", "Whiskers", "Snow", "Cuddle", "Fluffy", "Neko", "Meow", "Whisker", "Moonlight", "Tail",
+    "Sakura", "Kawaii", "Mochi", "Heart", "Dreamer", "Star", "Senpai", "Yume", "Kitsune", "Amai",
+    "Lux", "Ahri", "Jinx", "Ezreal", "Yasuo", "Riven", "Katarina", "Poppy", "Zoe", "MissFortune",
+    "Venti", "Klee", "Diluc", "Childe", "Lumine", "Xiao", "Jean", "Ganyu", "Keqing", "Noelle",
+    "HelloKitty", "MyMelody", "Cinnamoroll", "Pompompurin", "Kuromi", "Keroppi", "LittleTwinStars",
+    "Bubble", "Cupcake", "Sugar", "Starry", "Glitter", "Marshmallow", "Rainbow", "Fairy", "Bunny",
+    "Sleepy", "Milk", "Cotton", "Jelly", "Button", "Cinnamon", "Dreamy", "Sparkle", "Blossom", "Flora", 
+    "Petal", "Rose", "Lily", "Tulip", "Sunshine", "Lavender", "Clover", "Ivy", "Breeze", "Sky", "Cloud", 
+    "Starshine", "Moonbeam", "Galaxy", "Nova", "Starlight", "Glow", "Dawn", "Angel", "Sweetie", "Joy", 
+    "Happy", "Honey", "Maple", "Peanut", "Buttercup", "Snowflake", "Twinkle", "Panda", "Bear", "Cub", 
+    "Fox", "Wolf", "Lion", "Tiger", "Bumble", "Puff", "Frost", "Bliss", "Magic", "Wish", "Misty", 
+    "Whisper", "Shimmer", "Crystal", "Ocean", "Wave", "Shell", "Pearl", "Luna", "Sunny", "Glory", 
+    "Starfish", "Coral", "River", "Leaf", "Fern", "Tide", "Autumn", "Aurora", "Winter", "Echo", 
+    "Mystic", "Celeste", "Opal", "Jewel", "Serenity", "Sapphire", "Raine", "Feather", "Ash", "Ember", 
+    "Flare", "Nova", "Blitz", "Cascade", "Silver", "Dust", "Poppy", "Blush", "Twirl", "Velvet", 
+    "Harmony", "Fawn", "Butterfly", "Daisy", "Petunia", "Hazel", "Clara", "Violet", "Meadow", 
+    "Pine", "Glacier", "Sprout", "Fable", "Meadow", "Rosebud", "Snowdrop", "Pebble", "Icicle", "Brisk", 
+    "Rainy", "Shady", "Sunray", "Snowbell", "Cloudy", "Windy", "Moonshadow", "Sunkiss", "Petite", "Glint", 
+    "Moonmist", "Ivory", "Frosty", "Pixie", "Wisteria", "Iris", "Briar", "Lace", "Topaz", "Mint", 
+    "Jade", "Flare", "Vesper", "Solstice", "Dew", "Petra", "Harbor", "Basil", "Cedar", "Flicker", 
+    "Plume", "Ripple", "Cinder", "Faye", "Swan", "Pinecone", "Mauve", "Bliss", "Twilight", "Aspen", 
+    "Lyric", "Caspian", "Dewdrop", "Sable", "Violet", "Dream", "Wander", "Dove", "Whimsy", "Lilac", 
+    "Coral", "Sunny", "Shadow", "Moonlit", "Windsong", "Zephyr", "Seabreeze", "Glimmer", "Frostbite", 
+    "Eclipse", "Storm", "Crimson", "Carmine", "Rouge", "Starfall", "Lullaby", "Delight", "Dandelion", 
+    "Sunray", "Bramble", "Vine", "Spritz", "Brisk", "Sprinkle", "Mimosa", "Wren", "Canary", "Pansy", 
+    "Orchid", "Dahlia", "Blissful", "Lively", "Opal", "Zephyr", "Nimbus", "Aurora", "Eden", "Fawn", 
+    "Flicker", "Sonnet", "Talon", "Willow", "Velvet", "Whisper", "Sparrow", "Wisteria", "Flick", 
+    "Spritz", "Opal", "Serene", "Lark", "Whimsy", "Aurora", "Nimbus", "Serene", "Breeze", "Mirth", 
+    "Tranquil", "Verdant", "Sierra", "Echo", "Wisp", "Seraph", "Tundra", "Celestial", "Tempest", 
+    "Cypress", "Astra", "Reverie", "Daydream", "Twilight", "Breeze", "Serenity", "Glade", "Aurora", 
+    "Cascade", "Sylvan", "Valley", "Meadow", "Clover", "Ember", "Poppy", "Coral", "Dove", "Winter", 
+    "Raven", "Ivy", "Jade", "Pine", "Aspen", "Willow", "Frost", "Feather", "Briar", "Pearl", "Lace", 
+    "Starry", "Rosewood", "Moss", "Lotus", "Fawn", "Lilac", "Haze", "Velvet", "Flurry", "Thistle", 
+    "Opaline", "Luster", "Shimmer", "Carmine", "Citrine", "Nettle", "Mistral", "Rivulet", "Drift", 
+    "Aether", "Frostbite", "Blush", "Amethyst", "Topaz", "Teal", "Aqua", "Harbor", "Willow", 
+    "Mist", "Wave", "Drift", "Pebble", "Basil", "Zephyr"
+]
+
 };
 
 // Prefix and Suffix list for mixing
